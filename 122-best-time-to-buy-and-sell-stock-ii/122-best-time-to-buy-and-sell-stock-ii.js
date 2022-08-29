79cb001a4dd8df8e53cd3952abcf6edda45220bd
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-08-29 16:28:41 -0400

    Time: 104 ms (32.36%), Space: 42.1 MB (86.69%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+
+// for each element in prices,
+// call helper function / inner function
+// that....
+// check each element in the array if bigger than first element
+// if bigger, then as result, add difference of two + recursive call of remainding array
+var maxProfit = function(prices) {
+    let total = 0;
+    for (let i = 1; i < prices.length; i++) {
+        // check if bigger than previous, if so, add difference to total
+        if (prices[i] > prices[i - 1]) {
+            total += prices[i] - prices[i - 1];
+        }
+    }
+    return total;
+};
