1735c666c102380c2a6ccee1f3ac641f832ce82b
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-08-31 19:51:14 -0400

    Time: 178 ms (29.61%), Space: 51.4 MB (26.81%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,17 +3,17 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-//     const tracker = {};
-//     for (let i = 0; i < nums.length; i++) {
-//         if (tracker[nums[i]]) return true;
-//         tracker[nums[i]] = true;
-//     }
-//     return false;
-    
-    // sort, check if next element is duplicate
-    nums = nums.sort();
+    const tracker = {};
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === nums[i+1]) return true;
+        if (tracker[nums[i]]) return true;
+        tracker[nums[i]] = true;
     }
     return false;
+    
+    // sort, check if next element is duplicate
+    // nums = nums.sort();
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] === nums[i+1]) return true;
+    // }
+    // return false;
 };
