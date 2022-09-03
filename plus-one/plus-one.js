e79dc6a2786d8f638d03e19f30f9c93f202c6456
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-09-03 18:21:02 -0400

    Time: 129 ms, Memory: 41.8 MB - LeetHub

--- plus-one/plus-one.js
+++ plus-one/plus-one.js
@@ -16,16 +16,15 @@
 
 
 var plusOne = function(digits) {
-    let targetIndex = digits.length - 1;
-    let targetValue = ++digits[targetIndex];
-    while (targetValue > 9) {
-        digits[targetIndex] = 0;
-        if (targetIndex === 0) {
-            digits.unshift(1);
-            targetValue = 1;
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] === 9) {
+            digits[i] = 0;
+            if (i === 0) {
+                digits.unshift(1);
+            }
         } else {
-            targetIndex--;
-            targetValue = ++digits[targetIndex];
+            digits[i] = digits[i] + 1;
+            break;
         }
     }
     return digits;
