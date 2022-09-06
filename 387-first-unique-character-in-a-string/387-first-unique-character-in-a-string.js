a4b209e5491bf0ccb2d22990a3f435e878002cb5
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-09-06 15:45:12 -0400

    Time: 201 ms (19.09%), Space: 45.6 MB (46.69%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -6,14 +6,13 @@
 var firstUniqChar = function(s) {
     const tracker = {};
     for (let i = 0; i < s.length; i++) {
-        tracker[s[i]] = tracker[s[i]] ? [2, i] : [1, i];
-    }
-    let minIndex = Infinity;
-    for (let key in tracker) {
-        if (tracker[key][0] === 1) {
-            minIndex = Math.min(minIndex, tracker[key][1]);
-        }
+        tracker[s[i]] = tracker[s[i]] ? tracker[s[i]] + 1 : 1;
     }
     
-    return minIndex === Infinity ? -1 : minIndex;
+    for (let i = 0; i < s.length; i++) {
+        if (tracker[s[i]] === 1) return i;
+    }
+    
+    return -1
+    
 };
