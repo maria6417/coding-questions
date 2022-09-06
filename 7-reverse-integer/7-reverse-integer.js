701219a0ca314a14e94629e330c6e9eedc245d3c
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-09-06 15:23:14 -0400

    Time: 115 ms (42.71%), Space: 44.6 MB (12.71%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.js
+++ 7-reverse-integer/7-reverse-integer.js
@@ -7,9 +7,31 @@
 // reverse
 var reverse = function(number) {
     
-    let numArr = Array.from(number.toString());
-    let isNegative = numArr[0] === '-' ? numArr.splice(0, 1) : '';
-    let reversed = Number(isNegative + numArr.reverse().join(''));
+    // let numArr = Array.from(number.toString());
+    // let isNegative = numArr[0] === '-' ? numArr.splice(0, 1) : '';
+    // let reversed = Number(isNegative + numArr.reverse().join(''));
+    // if (reversed >= 2**31 || reversed <= -(2**31) + 1) return 0;
+    // return reversed
+    
+    const numArr = Array.from(number.toString());
+    // []
+    let reversed = [];
+    
+    // 3
+    while (numArr.length) {
+        reversed.push(numArr.pop());
+        // reversed = [3,2,1]
+    }
+    
+    if (reversed[reversed.length - 1] === '-') {
+        reversed.pop();
+        reversed = '-' + reversed.join('');
+    } else {
+        // 321
+        reversed = reversed.join('');
+    }
+
     if (reversed >= 2**31 || reversed <= -(2**31) + 1) return 0;
-    return reversed
+    
+    return reversed;
 };
