ef03b440780ab21c67623c057cb3aff14e410f26
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-09-12 12:22:55 -0400

    Time: 131 ms (27.82%), Space: 44.5 MB (75.63%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    // convert s to include alphanumeric letters
+    // using ascii?
+    s = s.toLowerCase().replace(/[^a-z\d]/g, '');
+    console.log(s);
+    
+    // if string length is less than 2 return true
+    if (s.length <= 1) return true;
+    // define starting pointer & ending pointer
+    let p1 = 0;
+    let p2 = s.length - 1;
+    
+    while (p1 < p2) {
+        if (s[p1++] !== s[p2--]) return false;
+    }
+    
+    return true;
+    // iterate through new s while starting pointer is smaller than ending pointer
+    // check that starting pointer letter is same as ending, if not return false
+    // increment starting, decrement ending pointer
+    
+    // if iteration ends, return true
+};
