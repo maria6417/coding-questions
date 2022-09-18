b9bab71752d780e52aafdf9134e9f915e2153a20
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-09-17 21:33:33 -0700

    Time: 85 ms (93.34%), Space: 42.5 MB (93.98%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    // strategy 1: keep track of characters using object
+    const tracker = {};
+    for (let i = 0; i < s.length; i++) {
+        tracker[s[i]] = (tracker[s[i]] | 0) + 1;
+    }
+    for (let i = 0; i < t.length; i++) {
+        if (!tracker[t[i]]) return false
+        tracker[t[i]]--;
+    }
+    return true;
+    // strategy 2: sort both strings & compare
+
+};
