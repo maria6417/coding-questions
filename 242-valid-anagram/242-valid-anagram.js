615e444e22ad3af38a606db4aae3c94f893932a4
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-09-17 21:34:45 -0700

    Time: 136 ms (41.54%), Space: 42.5 MB (92.43%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -9,20 +9,20 @@ var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
     
     // strategy 1: keep track of characters using object
-    // const tracker = {};
-    // for (let i = 0; i < s.length; i++) {
-    //     tracker[s[i]] = (tracker[s[i]] | 0) + 1;
-    // }
-    // for (let i = 0; i < t.length; i++) {
-    //     if (!tracker[t[i]]) return false
-    //     tracker[t[i]]--;
-    // }
-    // return true;
+    const tracker = {};
+    for (let i = 0; i < s.length; i++) {
+        tracker[s[i]] = (tracker[s[i]] | 0) + 1;
+    }
+    for (let i = 0; i < t.length; i++) {
+        if (!tracker[t[i]]) return false
+        tracker[t[i]]--;
+    }
+    return true;
     
     // strategy 2: sort both strings & compare
-    s = [...s].sort().join('');
-    t = [...t].sort().join('');
-    if (s !== t) return false;
-    return true;
+    // s = [...s].sort().join('');
+    // t = [...t].sort().join('');
+    // if (s !== t) return false;
+    // return true;
 
 };
