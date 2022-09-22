6443c7ae17099cc60eb83747ccbe81d52cfa9122
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-09-22 09:52:39 -0700

    Time: 119 ms (45.31%), Space: 44.3 MB (82.78%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,24 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    // convert s to include alphanumeric letters
-    // using ascii?
-    s = s.toLowerCase().replace(/[^a-z\d]/g, '');
-    
-    // if string length is less than 2 return true
-    if (s.length <= 1) return true;
-    // define starting pointer & ending pointer
-    let p1 = 0;
-    let p2 = s.length - 1;
-    
-    while (p1 < p2) {
-        if (s[p1++] !== s[p2--]) return false;
+    // replace non-alphanumeric chars with empty space
+    s = s.replace(/[^\da-z]/gi, '').toLowerCase();
+    if (s.length === 0) return true;
+    // convert s to lowercase
+    // start & end pointers
+    let start = 0;
+    let end = s.length - 1;
+    // while start is smaller than end
+    while(start < end) {
+        if (s[start++] !== s[end--]) return false;
     }
-    
+    // compare each chars, return false if mismatch
+    // increment start, decrement end
     return true;
-    // iterate through new s while starting pointer is smaller than ending pointer
-    // check that starting pointer letter is same as ending, if not return false
-    // increment starting, decrement ending pointer
-    
-    // if iteration ends, return true
 };
