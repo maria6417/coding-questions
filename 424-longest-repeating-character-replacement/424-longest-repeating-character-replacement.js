1cf93beda8228ed1184d9b2554be46cf4b7448d6
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-09-26 10:54:00 -0700

    Time: 433 ms (20.60%), Space: 48.3 MB (24.42%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -12,29 +12,38 @@
 var characterReplacement = function(s, k) {
     const freq = {}; //{A: 1, B:1}
     let maxSeq = 0;
-    let left = 0, right = 0; //1
-    freq[s[right]] = (freq[s[right]] | 0) + 1;
-    while (left < s.length && right < s.length) { //0
-        // check which letter is most freq
-        let max = 0; //1
-        let maxLetter = ''; //"A"
-        for (let key in freq) {
-            if (max <= freq[key]) {
-                max = freq[key];
-                maxLetter = key;
-            }
+    let l = 0 //1
+    for (let r = 0; r < s.length; r++) {
+        freq[s[r]] = (freq[s[r]] || 0) + 1;
+        // while window is not valid, decrement left freq, and increment left pointer
+        // window invalid = (l - r + 1) - max freq > k
+        while(r - l + 1 - (Math.max(...Object.values(freq)) || 0) > k) {
+            freq[s[l]]--;
+            l++;
         }
-        
-        if (right + 1 - left - max <= k) {
-            maxSeq = Math.max(maxSeq, right + 1 - left);
-            right++;
-            freq[s[right]] = (freq[s[right]] | 0) + 1;
-        } else {
-            freq[s[left]]--;
-            left++;
-        }
+        // when window is valid, update max
+        maxSeq = Math.max(maxSeq, r - l + 1);
     }
+//     while (left < s.length && right < s.length) { //0
+//         // check which letter is most freq
+        
+//         let max = 0; //1
+//         let maxLetter = ''; //"A"
+//         for (let key in freq) {
+//             if (max <= freq[key]) {
+//                 max = freq[key];
+//                 maxLetter = key;
+//             }
+//         }
+        
+//         if (right + 1 - left - max <= k) {
+//             maxSeq = Math.max(maxSeq, right + 1 - left);
+//             right++;
+//             freq[s[right]] = (freq[s[right]] | 0) + 1;
+//         } else {
+//             freq[s[left]]--;
+//             left++;
+//         }
+//     }
     return maxSeq;
 };
-
-characterReplacement("AABABBA", 1);
