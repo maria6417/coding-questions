c94ab3c69cf772cdb5a80783cdb827eed269d1b1
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-10-03 10:47:52 -0700

    Time: 115 ms (27.61%), Space: 43.3 MB (18.10%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -7,14 +7,20 @@
 // if letter is (, {, [, accumulate on stack
 // otherwise, check if stack has other side if not return false
 var isValid = function(s) {
+    const chars = {
+        ')' : '(',
+        '}' : '{',
+        ']' : '[',
+    };
     let stack = [];
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+        if (Object.values(chars).includes(s[i])) {
             stack.push(s[i]);
         } else {
-            if (s[i] === ')' && stack[stack.length - 1] !== '(') return false;
-            if (s[i] === '}' && stack[stack.length - 1] !== '{') return false;
-            if (s[i] === ']' && stack[stack.length - 1] !== '[') return false;
+            if (stack[stack.length - 1] !== chars[s[i]]) return false;
+            // if (s[i] === ')' && stack[stack.length - 1] !== '(') return false;
+            // if (s[i] === '}' && stack[stack.length - 1] !== '{') return false;
+            // if (s[i] === ']' && stack[stack.length - 1] !== '[') return false;
             stack.pop();
         }
     }
