24d17fe7a7652e1e2c45337f1644b6ab89dd14df
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-10-04 23:33:08 -0700

    Time: 10449 ms (5.01%), Space: 88.2 MB (5.11%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.js
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -16,33 +16,62 @@
     // keep track with object, key as temperature, value as array of appeared indexes
 
 var dailyTemperatures = function(temperatures) {
-    if (temperatures.length === 1) return [0];
-    const appeared = {};
+//     if (temperatures.length === 1) return [0];
+//     const appeared = {};
+//     const result = [];
+//     for (let i = 1; i < temperatures.length; i++) {
+//         if (temperatures[i] > temperatures[i - 1]) {
+//             result[i - 1] = 1;
+//             for (let key in appeared) {
+//                 if (key < temperatures[i]) {
+//                     for (let j = 0; j < appeared[key].length; j++) {
+//                         result[appeared[key][j]] = i - appeared[key][j];
+//                     }
+//                     delete appeared[key];
+//                 }
+//             }
+//         } else {
+//             appeared[temperatures[i - 1]] ? appeared[temperatures[i -1]].push(i - 1) : appeared[temperatures[i - 1]] = [i - 1];
+//         }
+//     }
+    
+//     for (let key in appeared) {
+//         for (let j = 0; j < appeared[key].length; j++) {
+//             result[appeared[key][j]] = 0;
+//         }
+//         delete appeared[key];
+//     }
+//     while (result.length !== temperatures.length) {
+//         result.push(0);
+//     }
+//     return result;
+    
+    const stack = [];
     const result = [];
-    for (let i = 1; i < temperatures.length; i++) {
-        if (temperatures[i] > temperatures[i - 1]) {
-            result[i - 1] = 1;
-            for (let key in appeared) {
-                if (key < temperatures[i]) {
-                    for (let j = 0; j < appeared[key].length; j++) {
-                        result[appeared[key][j]] = i - appeared[key][j];
-                    }
-                    delete appeared[key];
+    
+    for (let i = 0; i < temperatures.length; i++) {
+        // if top of stack is empty, or if top of stack > current element, assign value to same index of stack
+
+            while (stack[stack.length - 1] < temperatures[i]) {
+                result[stack.length - 1] = i - stack.length + 1;
+                stack.pop();
+                while(stack.length && !stack[stack.length - 1]) {
+                    stack.pop();
                 }
             }
-        } else {
-            appeared[temperatures[i - 1]] ? appeared[temperatures[i -1]].push(i - 1) : appeared[temperatures[i - 1]] = [i - 1];
-        }
+        stack[i] = temperatures[i];
+        // while top of stack < current element
+            // assign result's index value as i - stack's top index
+            // pop element out
     }
     
-    for (let key in appeared) {
-        for (let j = 0; j < appeared[key].length; j++) {
-            result[appeared[key][j]] = 0;
+    while (stack.length) {
+        if (stack[stack.length - 1]) {
+            result[stack.length - 1] = 0;
         }
-        delete appeared[key];
+        stack.pop();
     }
-    while (result.length !== temperatures.length) {
-        result.push(0);
-    }
+    // while stack is not empty, assign 0 to result's same index
+    
     return result;
 };
