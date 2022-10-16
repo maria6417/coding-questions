d5288988a358f72d2c8c07e4084af3d51acc7d23
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-10-16 00:38:36 -0700

    Time: 74 ms (86.77%), Space: 44.5 MB (31.78%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,21 +10,18 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    if (!head) return head;
+    if (!head || !head.next) return head;
     // continue pushing the node to stack until the next is null.
     // take out from stack, and keep track of the first as head
     // set it's next to the next on stack if exists, if not, set as null
     // return new head
     
-    const stack = [];
-    while(head) {
-        stack.push(head);
-        head = head.next;
-    }
-    let newHead = stack[stack.length - 1];
-    while (stack.length) {
-        head = stack.pop();
-        head.next = stack[stack.length - 1] || null;
-    }
+    // recursively
+    // return itself when the next is null
+    // otherwise, set its next to
+    const newHead = reverseList(head.next);
+    let curr = head.next;
+    head.next = null;
+    curr.next = head;
     return newHead;
 }; 
