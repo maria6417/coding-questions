b0f6d04e9a92fddb99e1f8b94d461a5ebf6802f5
Maria Hirai <33907509+maria6417@users.noreply.github.com>
2022-10-28 10:03:01 -0700

    Time: 134 ms (23.50%), Space: 44.8 MB (66.87%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,10 +11,18 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    while(head && head.val !== null) {
-        if (head.val === 'seen') return true;
-        head.val = 'seen';
-        head = head.next;
+    // while(head && head.val !== null) {
+    //     if (head.val === 'seen') return true;
+    //     head.val = 'seen';
+    //     head = head.next;
+    // }
+    // return false;
+    
+    let slow = head, fast = head?.next?.next || null;
+    while (slow && fast) {
+        if (slow === fast) return true;
+        slow = slow.next;
+        fast = fast?.next?.next || null;
     }
     return false;
 };
